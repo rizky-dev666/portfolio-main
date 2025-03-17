4ee4749167c8731ecf0d3f3fa6c2d8eb6bd4eaf2
rizky-dev666 <rizkyadilald12@gmail.com>
2025-03-17 20:41:38 +0700

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I focus on client collaboration and fulfilling their needs ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible with client time zone communication",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My skill set",
+    description: "I am continuously improving my skills",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Technology and AI enthusiasts for the future.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently diving deeper into front-end development",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Innovation Place - Competition Information Website",
+    des: "Website for national and international competition information.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fig.svg"],
+    link: "",
+  },
+  {
+    id: 2,
+    title: "Donor Yuk! - Blood Donation App",
+    des: "An application to facilitate blood donation that can be accessed anywhere.",
+    img: "/p2.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fig.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "TEES - Second-Hand Trade",
+    des: "E-commerce website for buying, selling or exchanging used goods.",
+    img: "/p3.svg",
+    iconLists: ["/vite.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fig.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "PUTRA AR - E-commerce Website",
+    des: "E-commerce website for buying and selling food that is ready to be delivered throughout Indonesia",
+    img: "/p4.svg",
+    iconLists: ["/vite.svg", "/tail.svg", "/ts.svg", "/supa.svg", "/fig.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Rizky was an incredible experience. His expertise, responsiveness, and commitment to excellence were evident in every aspect of our project. Rizky's passion for development truly shines. If you’re looking to enhance your website and strengthen your brand, Rizky is the perfect choice.",
+    name: "Faiq Yusuf Raharja",
+    title: "Undergraduate Student",
+  },
+  {
+    quote:
+      "Collaborating with Rizky was seamless and rewarding. His professionalism, efficiency, and eye for detail made a significant impact on our project’s success. Rizky's dedication is unmatched. If you need a skilled developer to elevate your online presence, Rizky is the one to trust.",
+    name: "Farel Muamar",
+    title: "Undergraduate Student",
+  },
+  {
+    quote:
+      "Rizky is an exceptional professional. His ability to deliver outstanding results with precision and creativity exceeded our expectations. His enthusiasm for innovation is truly inspiring. If you're looking for a developer who brings both skill and passion, Rizky is the ideal partner.",
+    name: "Rivan Agustian",
+    title: "Undergraduate Student",
+  },
+  {
+    quote:
+      "Working with Rizky was a fantastic experience. His professionalism, problem-solving skills, and dedication to quality made our project smooth and successful. If you need a reliable developer who delivers outstanding results, Rizky is the right choice.",
+    name: "Rendi Nurfauzi",
+    title: "Undergraduate Student",
+  },
+  {
+    quote:
+      "Rizky's expertise and commitment to excellence set him apart. His ability to turn ideas into reality with precision and creativity is truly impressive. If you want to take your website and brand to the next level, Rizky is the perfect collaborator.",
+    name: "Gilang Ramdhani",
+    title: "Undergraduate Student",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "supabase",
+    img: "/sup.svg",
+    nameImg: "/supaName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer",
+    desc: "Assisted in the development of a web-based platform using Vite, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance UI Designer",
+    desc: "Designing an e-commerce website for an individual business.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/rizky-dev666",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/rizkyad__?igsh=MTlxbzE3NWxkNW9oZg==",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/rizky-adilal-danawan-9a5671340?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  },
+];
