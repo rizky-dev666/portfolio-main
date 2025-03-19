e3b1b0add7d83e15846a12ef783c702267dc8111
rizky-dev666 <rizkyadilald12@gmail.com>
2025-03-19 14:51:56 +0700

    set experience

--- data/index.ts
+++ data/index.ts
@@ -188,7 +188,7 @@ export const workExperience = [
   {
     id: 3,
     title: "Freelance UI Designer",
-    desc: "Designing an e-commerce website for an individual business.",
+    desc: "Designing a user-friendly e-commerce site for an individual business to boost sales and visibility.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
